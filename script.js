74f9280d4c01319d77bc3c4ed2584b54c18a492f
Besiki Gugushvili <74554872+beska0013@users.noreply.github.com>
2021-01-31 01:49:36 +0400

    IN PROGRESS

---
+++ script.js
@@ -0,0 +1,24 @@
+function bodyColor(){
+    const arr='0123456789ABCDF'
+    arr.split('')
+    
+    for(let i=0;i<=arr.length;i++){
+        let arrColor='#'+[arr[i],arr[i],arr[i],arr[i],arr[i],arr[i]] 
+         arrColor.split(' ')
+        console.log(randomColor)
+
+    }
+}
+
+
+bodyColor()
+
+
+
+
+
+
+// const arr = ["akaki", "john", "tom", "david", "Jason", "Hello"];
+// const randomNum = Math.floor(Math.random() * arr.length);
+// const res = arr[randomNum];
+// console.log(res);
